a814d2f62b4a3b03d84e352970937ebd42aabbdd
Yash-Khatri-31 <yashr124@gmail.com>
2022-02-15 14:38:09 +0400

    added an index

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// const Slack = require('slack-node');
+
+// const token = "xoxb-3103804815844-3090836486951-E6oNLfteiLIZmgODt3nZg0Zq"
+
+// const slack = new Slack(token);
+
+// slack.api('chat.postMessage', {
+//     text:'Yo',
+//     channelid:'C032NCAKJPR'
+// },function(err,res){
+//     console.log(res)
+// });
+
+// // const {WebClient, ErrorCode} = require('@slack/web-api');
+
+// // const web = new WebClient(token);
+
+// // const channelId = 'C032NCAKJPR';
+
+// // (async () => {
+// //     const result = await web.chat.postMessage({
+// //         text:'yo',
+// //         channel:channelId
+// //     })
+// //     console.log(`${result.ts}`)
+// // })();
