e07897ea0bca2716a3b480f0a173b180b71efd8a
Yash-Khatri-31 <yashr124@gmail.com>
2022-02-18 00:20:21 +0400

    updated

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-const text = 'Hello Sir';
+const yaml = require('js-yaml');
+const fs = require('fs');
 
-module.exports = text;
+const {WebClient} = require('@slack/web-api')
+const {createEventAdapter} = require('@slack/events-api')
 
+const port = 8000;
 
+const slackEvent = createEventAdapter('3c0f4d61d01a5b207c7135018a6f63f9');
+const client = new WebClient('xoxb-3103804815844-3092172404727-AzWbChFBRDLyXX2dXtFajMD3');
 
+slackEvent.on('app_mention',(event) => {
+    console.log(`Message is ${event.text} `);
 
+    // let mainText = event.text;
+    // let route  = yaml.safeLoad(fs.readFileSync('./.github/workflows/main.yml', 'utf8'));
+    // route.jobs.build.steps.with.slackmessage = mainText ;
+    // console.log(mainText);
+    // console.log(route.jobs.build.steps.with.slackmessage)
+    // fs.writeFile('./.github/workflows/main.yml', yaml.safeDump(route));
 
+    (async () => {
+        try{
+            await client.chat.postMessage({channel:event.channel , text:event.text})
+        }catch(error){
+            console.log(error.data)
+        }
+    })();
 
+});
 
+slackEvent.on('error',console.error)
 
-
+slackEvent.start(port).then(() => {
+    console.log(`listening to port ${port}`)
+});
 
 
 // const Slack = require('slack-node');
