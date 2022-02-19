f18f14cebd7733a83fa29cd4688c09f753474e6a
Yash-Khatri-31 <yashr124@gmail.com>
2022-02-19 12:08:13 +0400

    Update

--- index.js
+++ index.js
@@ -1,57 +1,24 @@
 
+const { exec } = require('child_process');
+
 const {WebClient} = require('@slack/web-api')
 const {createEventAdapter} = require('@slack/events-api')
 
+require('dotenv').config()
+
 const port = 8000;
 
-const slackEvent = createEventAdapter('3c0f4d61d01a5b207c7135018a6f63f9');
-const client = new WebClient('xoxb-3103804815844-3092172404727-AzWbChFBRDLyXX2dXtFajMD3');
+const slackEvent = createEventAdapter(process.env.SIGNIN_SECRET);
+const client = new WebClient(process.env.SLACK_BOT_TOKEN);
 
-slackEvent.on('app_mention',(event) => {
-    console.log(`Message is ${event.text} `);
-    (async () => {
-        try{
-            let count = 0;
-            const response = await client.chat.postMessage({channel:event.channel , text:event.text})
-            if(count===1) break;
-            else count++;
-        }catch(error){
-            console.log(error.data)
-        }
-    })();
+slackEvent.on('message',(event) => {
+    if(event.text === 'deploy'){
 
-});
+    }
+})
 
 slackEvent.on('error',console.error)
 
 slackEvent.start(port).then(() => {
     console.log(`listening to port ${port}`)
 });
-
-
-// const Slack = require('slack-node');
-
-// const token = "xoxb-3103804815844-3090836486951-E6oNLfteiLIZmgODt3nZg0Zq"
-
-// const slack = new Slack(token);
-
-// slack.api('chat.postMessage', {
-//     text:'Yo',
-//     channelid:'C032NCAKJPR'
-// },function(err,res){
-//     console.log(res)
-// });
-
-// // const {WebClient, ErrorCode} = require('@slack/web-api');
-
-// // const web = new WebClient(token);
-
-// // const channelId = 'C032NCAKJPR';
-
-// // (async () => {
-// //     const result = await web.chat.postMessage({
-// //         text:'yo',
-// //         channel:channelId
-// //     })
-// //     console.log(`${result.ts}`)
-// // })();
