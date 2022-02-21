e18fe54120f8eb32b77e166d8169fbb5f7959bdc
Yash-Khatri-31 <yashr124@gmail.com>
2022-02-21 11:51:45 +0400

    Final working with octokit

--- check.js
+++ check.js
@@ -1,6 +1,7 @@
 const {exec} = require('child_process');
 
-exec(process.env.CODE, {cwd: 'D:/NODEJSCOURSE/GA'},(error,stdout,stderr) => {
-    if(error) console.log(error)
-    console.log(stdout)
+exec(`curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${process.env.CODE}" https://api.github.com/repos/Yash-Khatri-31/GA/actions/workflows/main.yml/dispatches -d "{\"ref\":\"main\"}"`,{cwd: 'D:/NODEJSCOURSE/GA'},(error,stdout,stderr) => {
+            if(error) console.log(error)
 })
+
+// curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token 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" https://api.github.com/repos/Yash-Khatri-31/GA/actions/workflows/main.yml/dispatches -d "{\"ref\":\"main\"}"
--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const {WebClient} = require('@slack/web-api')
 const {createEventAdapter} = require('@slack/events-api')
 const {exec} = require('child_process')
+const {Octokit} = require('@octokit/rest')
 
 require('dotenv').config()
 
 const port = 8000;
 
+const web = new WebClient(process.env.SLACK_BOT_TOKEN)
 const slackEvent = createEventAdapter(process.env.SIGNIN_SECRET);
+const octokit = new Octokit();
 // const client = new WebClient(process.env.SLACK_BOT_TOKEN);
 
-slackEvent.on('app_mention',(event) => {
+slackEvent.on('app_mention',async (event) => {
     let text = event.text;
     text = text.split(' ').slice(0,-1)
     text = text.join(' ')
     console.log(text);
 
     if(text === 'deploy'){
-        console.log('Pushing Code')
-        exec(`curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token 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" https://api.github.com/repos/Yash-Khatri-31/GA/actions/workflows/main.yml/dispatches -d '{"ref":"main"}'`,{cwd: 'D:/NODEJSCOURSE/GA'},(error,stdout,stderr) => {
-            if(error) console.log(error)
-        })
-        console.log('Done')
+        await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+            headers: {authorization: `token ${process.env.CODE}`},
+            owner: 'Yash-Khatri-31',
+            repo: 'GA',
+            workflow_id: 'main.yml',
+            ref: 'main'
+          })
     }
 })
 slackEvent.on('error',console.error)
@@ -28,6 +33,3 @@ slackEvent.on('error',console.error)
 slackEvent.start(port).then(() => {
     console.log(`listening to port ${port}`)
 });
-
-
-// -d '{"ref":"main"}'
