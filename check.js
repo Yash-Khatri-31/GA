92a512158d7f21371a83cc5d458d342e1ac31382
Yash-Khatri-31 <yashr124@gmail.com>
2022-02-22 11:36:57 +0400

    Final Code

--- check.js
+++
@@ -1,7 +0,0 @@
-const {exec} = require('child_process');
-
-exec(`curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${process.env.CODE}" https://api.github.com/repos/Yash-Khatri-31/GA/actions/workflows/main.yml/dispatches -d "{\"ref\":\"main\"}"`,{cwd: 'D:/NODEJSCOURSE/GA'},(error,stdout,stderr) => {
-            if(error) console.log(error)
-})
-
-// curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token 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" https://api.github.com/repos/Yash-Khatri-31/GA/actions/workflows/main.yml/dispatches -d "{\"ref\":\"main\"}"
